8702a5ce9013cf66fdcfba6ebb62745e4796c932
Amen <jiinmam12@naver.com>
2023-07-10 17:25:20 +0900

    dd

---
+++ spring0706/src/test/java/com/java/Spring0706ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring0706ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
